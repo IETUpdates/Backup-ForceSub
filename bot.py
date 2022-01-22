7092786a16d914c031b9bf19d993981eb06a827c
IET Updates <83505196+IETUpdates@users.noreply.github.com>
2022-01-22 11:55:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ User = Client(session_name=SESSION,
  
 @Bot.on_message(filters.private & filters.command("start"))
 async def start(bot, message):
-    await message.reply(f"<b>Hello {message.from_user.mention}!\nI will forward all files from your groups to backup channel\nAlso you can use me as a ForceSubscribe Bot!\n\nYou can make your own bot from this [SOURCE CODE](https://github.com/Arun-TG/Backup-ForceSub)</b>",
+    await message.reply(f"<b>Hello {message.from_user.mention}!\nI will forward all files from @IET_TorrCloud to backup channel\n\n© @IET_Owner</b>",
                         disable_web_page_preview=True)
 
 @Bot.on_message(filters.chat(GROUPS))
